d9364576a72344d220d4526f5445f99385b1449d
lholznagel <lars.holznagel@outlook.com>
2017-08-24 08:23:05 +0200

    updated media keys

--- i3/lock.sh
+++ i3/lock.sh
@@ -5,7 +5,7 @@ IMAGE=/tmp/i3lock.png
 
 BLURTYPE="8x9"
 
-dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Stop
+$HOME/.scripts/sp pause
 
 convert $IMAGE -level 0%,100%,0.6 -blur $BLURTYPE - $IMAGE
 i3lock -i $IMAGE
